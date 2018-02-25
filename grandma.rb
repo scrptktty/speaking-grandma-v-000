cfeae31a605b0bdeaa78d2015d1665d13b190ee8
Kachel Wittig <kjwittig@gmail.com>
2018-02-25 03:43:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -15,10 +15,11 @@
 def speak_to_grandma(phrase)
   if phrase.upper == true
     puts "NO, NOT SINCE 1938!"
-    
+
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN"
-  
-  else 
+
+  else
     puts "HUH? SPEAK UP, SONNY!"
+  end
 end
